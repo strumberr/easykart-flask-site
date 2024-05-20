c48257d6455365123ff0de6a645b2f27f295b426
Storm <stormurbjorn24@gmail.com>
2024-05-20 19:45:45 +0700

    updated api and user page

--- components/database.py
+++ components/database.py
@@ -3,6 +3,9 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 import requests
 from datetime import datetime
+import bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 load_dotenv()
 
@@ -98,26 +101,27 @@ def levenshtein_distance(s1, s2):
 
 
 def search_all_similar_usernames(username, threshold=2):
-    # Retrieve all usernames
-    response = supabase.table('participants').select('username').execute()
+    # Retrieve all usernames and their availability
+    response = supabase.table('participants').select('username', 'username_availability').execute()
     
     if not response.data:
         return []
 
-    usernames = [record['username'] for record in response.data]
+    # Extract usernames and availability
+    usernames = [(record['username'], record['username_availability']) for record in response.data]
 
     # Calculate Levenshtein distance and partial match
     similar_usernames = []
-    for name in usernames:
+    for name, availability in usernames:
         distance = levenshtein_distance(username.lower(), name.lower())
         if username.lower() in name.lower() or distance <= threshold:
-            similar_usernames.append((name, distance))
+            similar_usernames.append((name, availability, distance))
     
     # Sort by similarity (lower distance is more similar)
-    similar_usernames.sort(key=lambda x: x[1])
+    similar_usernames.sort(key=lambda x: x[2])
     
-    # Return only the usernames
-    return [name for name, distance in similar_usernames]
+    # Return the list of tuples containing username and availability
+    return [(name, availability) for name, availability, distance in similar_usernames]
 
 
 def getBestScoreUsername(username, category):
@@ -142,38 +146,32 @@ def getLastPlace(category):
     return response.data[0]
 
 
+
 def detailedUserStats(username):
     categories = ['kids', 'normal', 'adults']
-    stats = {
-        "all_records": []
-    }
+    stats = {"all_records": {}}
     
     for category in categories:
-        
         bestScore = getBestScoreUsername(username, category)
+        first_place = getFirstPlace(category)
+        total_drivers = getTotalDrivers(category)
         
-        print(bestScore)
+        # get total drivers for the category from supabase
+
         
-        record_count = 0
+        
         records = []
         
         for record in bestScore:
-            date = float(record['score'])
-
             date = record['date']
             formatted_date = date.split("T")[0]
             date_object = datetime.strptime(formatted_date, '%Y-%m-%d')
             day_of_week = date_object.strftime('%A')
             
-            print(getTotalDrivers(category))
+            player_top_percentage = (record['position'] / total_drivers) * 100
+            percentage_to_beat_first = ((record['score'] - first_place['score']) / first_place['score']) * 100
+            seconds_to_beat_first = record['score'] - first_place['score']
 
-            player_top_percentage = getTotalDrivers(category) / record['position'] * 100
-
-            percentage_to_beat_first = (record['score'] - getFirstPlace(category)['score']) / getFirstPlace(category)['score'] * 100
-
-            seconds_to_beat_first = record['score'] - getFirstPlace(category)['score']
-
-            label = ""
             if player_top_percentage >= 80:
                 label = "Newbie"
             elif player_top_percentage >= 60:
@@ -189,30 +187,139 @@ def detailedUserStats(username):
             else:
                 label = "Track Owner"
 
-            seconds_to_beat_first = f"{abs(seconds_to_beat_first):.3f}"
-            player_top_percentage = f"{player_top_percentage:.2f}"
-            position = record['position']
-            formatted_date = formatted_date
-            score = record['score']
-            participant = username
-            percentage_to_beat_first = f"{percentage_to_beat_first:.2f}"
-
             records.append({
                 "label": label,
-                "seconds_to_beat_first": seconds_to_beat_first,
-                "player_top_percentage": player_top_percentage,
-                "position": position,
+                "seconds_to_beat_first": f"{abs(seconds_to_beat_first):.3f}",
+                "player_top_percentage": f"{player_top_percentage:.2f}",
+                "position": record['position'],
                 "date": formatted_date,
-                "score": score,
-                "participant": participant,
-                "percentage_to_beat_first": percentage_to_beat_first,
-                "day_of_week": day_of_week
+                "score": record['score'],
+                "participant": username,
+                "percentage_to_beat_first": f"{percentage_to_beat_first:.2f}",
+                "day_of_week": day_of_week,
+                "total_drivers": total_drivers,
             })
-            
-            record_count += 1
         
-        stats["all_records"].append({
-            category: records
-        })
-        
+        stats["all_records"][category] = records
+    
     return stats
+
+
+
+class Users:
+    def __init__(self):
+        pass
+    
+    def get_user_by_id(self, user_id):
+        response = supabase.table('users').select('id', 'email', 'username', 'username_easykart').eq('id', user_id).execute()
+        return response.data[0] if response.data else None
+    
+    def get_user_by_email(self, email):
+        response = supabase.table('users').select('id', 'email', 'username', 'username_easykart').eq('email', email).execute()
+        return response.data[0] if response.data else None
+    
+    def get_user_by_username(self, username):
+        response = supabase.table('users').select('id', 'email', 'username', 'username_easykart').eq('username', username).execute()
+        return response.data[0] if response.data else None
+    
+    def authenticate_user(self, email, password):
+        # get the hashed password using email from supabase
+        response = supabase.table('users').select('password').eq('email', email).execute()
+        if not response.data:
+            return False
+        
+        hashed_password = response.data[0]['password']
+        
+        # compare the hashed password with the password provided
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+    
+    
+
+    
+    def create_user(self, email, password, username, easykart_username):
+
+        user = self.get_user_by_email(email)
+        if user:
+            return None
+        
+        # check if the username is taken
+        user = self.get_user_by_username(username)
+        if user:
+            return { "error": "Username is already taken" }
+        
+        # check if username is available from participants
+        response = supabase.table('participants').select('username_availability').eq('username', easykart_username).execute()
+        if not response.data:
+            return { "error": "Username is not available" }
+        
+        # check if email is taken
+        user = self.get_user_by_email(email)
+        if user:
+            return { "error": "Email is already taken" }
+        
+        
+        # hash password
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        
+        # create user
+        response = supabase.table('users').insert({
+            "email": email,
+            "password": hashed_password.decode('utf-8'),
+            "username": username,
+            "username_easykart": easykart_username
+        }).execute()
+        
+        # mark the username as unavailable
+        response = supabase.table('participants').update({
+            "username_availability": False
+        }).eq('username', easykart_username).execute()
+        
+        
+        return response.data[0] if response.data else None
+    
+    def update_easykart_username(self, user_id, username_easykart):
+        response = supabase.table('users').update({
+            "username_easykart": username_easykart
+        }).eq('id', user_id).execute()
+        return response.data[0] if response.data else None
+    
+    
+    def get_easykart_username(self, user_id):
+        response = supabase.table('users').select('username_easykart').eq('id', user_id).execute()
+        return response.data[0] if response.data else None
+    
+    def connect_easykart(self, user_id, username_easykart):
+        response = supabase.table('users').update({
+            "username_easykart": username_easykart
+        }).eq('id', user_id).execute()
+        return response.data[0] if response.data else None
+    
+    
+    def search_all_available_similar_usernames(self, username, threshold=2):
+        # Retrieve all usernames
+        response = supabase.table('participants').select('username').eq('username_availability', True).execute()
+        
+        if not response.data:
+            return []
+
+        usernames = [record['username'] for record in response.data]
+
+        # Calculate Levenshtein distance and partial match
+        similar_usernames = []
+        for name in usernames:
+            distance = levenshtein_distance(username.lower(), name.lower())
+            if username.lower() in name.lower() or distance <= threshold:
+                similar_usernames.append((name, distance))
+        
+        # Sort by similarity (lower distance is more similar)
+        similar_usernames.sort(key=lambda x: x[1])
+        
+        # Return only the usernames
+        return [name for name, distance in similar_usernames]
+        
+    
+    def get_all_easykart_users(self, username):
+        # use the search function to get all similar usernames
+        similar_usernames = self.search_all_available_similar_usernames(username)
+        
+        return similar_usernames
