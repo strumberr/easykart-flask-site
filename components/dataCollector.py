b133fbd2a2e4274a2300ea87d9413ccf44657107
storm <stormurbjorn24@gmail.com>
2024-05-23 23:39:15 +0700

    requirements and data collection

--- components/dataCollector.py
+++ components/dataCollector.py
@@ -1,34 +1,33 @@
-import requests 
+import requests
 import json
-from authenticate import get_auth_bearer, get_access_token
-from database import *
+import threading
 import time
-
-auth_bearer = get_auth_bearer()
-kart_access_token = get_access_token(auth_bearer)
-
-kart_access_token = kart_access_token
-
-apis = {
-    "kids": f"https://modules-api6.sms-timing.com/api/besttimes/records/easykart?locale=ENG&rscId=242388&scgId=242396&startDate=1920-5-1+06%3A00%3A00&endDate=&maxResult=1000&accessToken={kart_access_token}",
-    "normal": f"https://modules-api6.sms-timing.com/api/besttimes/records/easykart?locale=ENG&rscId=242388&scgId=242392&startDate=1920-5-1+06%3A00%3A00&endDate=&maxResult=1000&accessToken={kart_access_token}",
-    "adults": f"https://modules-api6.sms-timing.com/api/besttimes/records/easykart?locale=ENG&rscId=242388&scgId=&startDate=1920-5-1+06%3A00%3A00&endDate=&maxResult=5000&accessToken={kart_access_token}"
-}
+import schedule
+from authenticate import get_auth_bearer, get_access_token
+from database import add_participants, insert_records
+from database import *
 
 
-records = fetch_data(apis["adults"])
-print(records)
+def job():
+    print("Fetching data...")
+    auth_bearer = get_auth_bearer()
+    kart_access_token = get_access_token(auth_bearer)
+
+    apis = {
+        "kids": f"https://modules-api6.sms-timing.com/api/besttimes/records/easykart?locale=ENG&rscId=242388&scgId=242396&startDate=1920-5-1+06%3A00%3A00&endDate=&maxResult=1000&accessToken={kart_access_token}",
+        "normal": f"https://modules-api6.sms-timing.com/api/besttimes/records/easykart?locale=ENG&rscId=242388&scgId=242392&startDate=1920-5-1+06%3A00%3A00&endDate=&maxResult=1000&accessToken={kart_access_token}",
+        "adults": f"https://modules-api6.sms-timing.com/api/besttimes/records/easykart?locale=ENG&rscId=242388&scgId=&startDate=1920-5-1+06%3A00%3A00&endDate=&maxResult=5000&accessToken={kart_access_token}"
+    }
+
+    for category, api_url in apis.items():
+        records = fetch_data(api_url)
+        add_participants(records)
+        insert_records(records, category)
 
 
-# while True:
-#     for category, api_url in apis.items():
-#         records = fetch_data(api_url)
-#         add_participants(records)
-#         insert_records(records, category)
-        
-#         time.sleep(60)
+schedule.every(12).hours.do(job)
 
-for category, api_url in apis.items():
-    records = fetch_data(api_url)
-    add_participants(records)
-    insert_records(records, category)
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
+
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+cd /home/projects/easykart-flask
+
+source .venv/bin/activate
+
+# Clean untracked files and directories
+git clean -fd
+
+# Pull the latest changes from the repository
+git pull git@github.com:strumberr/easykart-flask-site.git main
+
+# Install any new dependencies
+pip3 install -r requirements.txt
